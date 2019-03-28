5fcab7e32824f14b5f665b5bffdf01b0249662a4
Abhishek Chahar <abhishekchaharr@gmail.com>
2019-03-28 17:34:26 +0530

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerService } from './shared/customer.service';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -17,7 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ CustomerService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/customer.service.ts
+++ src/app/shared/customer.service.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 
 import { Injectable } from '@angular/core';
 
@@ -9,9 +9,10 @@ export class CustomerService {
 
   constructor() { }
   form = new FormGroup( {
-    fullname: new FormControl(''),
-    email: new FormControl(''),
-    mobile: new FormControl(''),
+    $key: new FormControl(null),
+    fullname: new FormControl('' , Validators.required),
+    email: new FormControl('', Validators.email),
+    mobile: new FormControl('',[ Validators.required, Validators.minLength(8)]),
     location: new FormControl(''),
   });
 }
