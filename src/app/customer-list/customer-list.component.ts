ddeb61690aae74bb96b704aa4b3e3a44d78bcead
Abhishek Chahar <abhishekchaharr@gmail.com>
2019-03-30 12:27:27 +0530

    working 2.0

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -7,9 +7,9 @@ import { CustomerService } from '../shared/customer.service';
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.css']
 })
-export class CustomerListComponent implements OnInit {
+export  class CustomerListComponent implements OnInit {
 
-  constructor( private customerService: CustomerService) { }
+  constructor( public customerService: CustomerService) { }
   customerArray = [];
   showDeleteMessage: boolean;
   searchText = '';
