25db34e9016e2a5a70c0bd9516d35a31cf1dc137
Abhishek Chahar <abhishekchaharr@gmail.com>
2019-05-05 12:15:10 +0530

    Adding service to credit component

--- src/app/transfer-credit/transfer-credit.component.ts
+++ src/app/transfer-credit/transfer-credit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../shared/customer.service';
 
 @Component({
   selector: 'app-transfer-credit',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfer-credit.component.css']
 })
 export class TransferCreditComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(public customerService: CustomerService) { }
+  searchname = '';
+  giveamount = 0;
+  amount1 =  0;
+  amount2 = 0;
   ngOnInit() {
   }
-
 }
